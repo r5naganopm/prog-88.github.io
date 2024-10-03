e61a25e19bf215086a328f3746b42c4fad15e73f
r5naganopm <suzuki-y@actec.or.jp>
2024-10-03 17:42:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 document.getElementById('exploreButton').addEventListener('click', function() {
-    alert('新しい冒険が始まります！');
-    // ここにさらなる機能を追加できます
+    const stories = [
+        "深夜の訪問者: あなたの部屋のドアが静かに開く...",
+        "消えた村の伝説: 誰も戻ってこなかった...",
+        "鏡の向こうの影: それはあなたを見ている...",
+    ];
+    const randomIndex = Math.floor(Math.random() * stories.length);
+    alert(stories[randomIndex]);
 });
